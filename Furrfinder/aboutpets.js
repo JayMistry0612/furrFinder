0e27c22c2b058fcce3cb7ebd4738f39e6b98a3a7
nikhilpatill18 <nikhilpatil6060@gmail.com>
2025-02-04 22:20:33 +0530

    added some new in navbar

--- Furrfinder/aboutpets.js
+++ Furrfinder/aboutpets.js
@@ -1,5 +1,7 @@
 const getPets = document.getElementById("about-pets");
 const navbarBottom = document.getElementById("navbar-bottom");
+const navbar_icon = document.getElementById("nav-icon");
+const sidenavBar = document.getElementById("side-navbar")
 getPets.addEventListener("click", () => {
     if (navbarBottom.classList.contains("hidden")) {
         navbarBottom.classList.toggle("hidden");
@@ -26,5 +28,36 @@ document.querySelectorAll(".nav-item").forEach((e) => {
     e.addEventListener("mouseleave", () => {
         e.childNodes[3].style.display = "none"
         document.querySelector("main").style.zIndex = ""
+    })
+})
+
+
+navbar_icon.addEventListener("click", () => {
+    if (sidenavBar.classList.contains("hidden")) {
+        sidenavBar.classList.toggle("hidden");
+        // sidenavBar.style.display = "flex ";
+        sidenavBar.style.opacity = "1"
+        sidenavBar.style.transform = "translateX(10px)"
+        sidenavBar.style.transform = "all 1s ease"
+    }
+    else {
+        sidenavBar.classList.toggle("hidden");
+        // sidenavBar.style.display = "none";
+        sidenavBar.style.opacity = "0"
+        sidenavBar.style.transform = "translateX(-100px)"
+        sidenavBar.style.transform = "all 0.1s ease"
+    }
+})
+document.querySelectorAll("#side-navbar .side-nav-item li").forEach((e) => {
+    e.addEventListener("click", () => {
+        if (e.children[0].classList.contains("hidden")) {
+            e.children[1].style.display = 'block'
+            e.children[0].classList.toggle("hidden")
+        }
+        else {
+            e.children[1].style.display = 'none'
+            e.children[0].classList.toggle("hidden")
+
+        }
     })
 })
